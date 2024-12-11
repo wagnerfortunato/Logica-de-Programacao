24b5d3aab09e121d5e42d63e392e9071a0cc56e4
wagnerfortunato <wagnerjonnatasjc@gmail.com>
2024-12-10 22:37:15 -0300

    Break

--- controle/desafio_switch.js
+++ controle/desafio_switch.js
@@ -8,3 +8,20 @@
 // E+ -> 2
 // E -> 1
 // F -> 0
+
+const conceito = 'F';
+
+switch(conceito) {
+    case 'A+': console.log(10); break;
+    case 'A': console.log(9); break;
+    case 'B+': console.log(8); break;
+    case 'C+': console.log(7); break;
+    case 'C': console.log(6); break;
+    case 'D+': console.log(5); break;
+    case 'D': console.log(4); break;
+    case 'E+': console.log(3); break;
+    case 'E': console.log(2); break;
+    case 'F': console.log(1); break;
+    default: console.log('Conceito invalido!');
+
+}
